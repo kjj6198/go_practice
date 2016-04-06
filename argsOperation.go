f76276dad28c634403d396b6f887c375c3d337b2
Kalan <kjj6198@gmail.com>
2016-04-06 14:18:46 +0800

    add args operation practice.

---
+++ argsOperation.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "path"
+)
+
+func main() {
+	// 如果一開始沒有賦值，會被隱式賦值
+	var result string
+	var resultWithName string
+	var sep string	
+	// 等價
+	// s := ""
+  
+
+	// go 裡面只允許 i++ 但不允許 ++i，也不允許賦值。
+		
+	for i:= 1; i < len(os.Args); i++ {
+	  result += sep + os.Args[i]
+	  sep = " "
+	}
+
+	fmt.Println(result)
+
+
+	for i:= 0; i < len(os.Args); i++ {
+	  resultWithName += sep + os.Args[i]
+	  sep = "\n"
+	}
+
+	// 1.1
+	// 1.2
+	fmt.Println(path.Base(resultWithName))
+
+	
+
+	// join expect a string array
+	
+}
